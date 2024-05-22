e701a169a6d5105c71f95de7bec8b63532cae5f9
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-05-21 18:45:51 -0600

    finishes functionality for flask api crud assignment

--- flask-api-crud/controllers/products_controller.py
+++ flask-api-crud/controllers/products_controller.py
@@ -7,11 +7,27 @@ def product_create(request):
     data = request.form if request.form else request.json
     product = {}
     product['product_id'] = data['product_id']
-    product['name'] = data['name']
+    product['product_name'] = data['product_name']
     product['description'] = data['description']
     product['price'] = data['price']
+    product['active'] = bool(data['active'])
     product_records.append(product)
-    return jsonify({"message": f"Product {product['name']} has been added."}), 200
+    return jsonify({"message": f"Product {product['product_name']} has been added."}), 200
+
+
+def products_get():
+    return jsonify(product_records), 200
+
+
+def products_get_active():
+    active_products = []
+    for product in product_records:
+        if product['active'] == True:
+            active_products.append(product)
+    if active_products:
+        return jsonify(active_products), 200
+    else:
+        return jsonify("Message: no active Products were found."), 404
 
 
 def product_get_by_id(product_id):
@@ -30,15 +46,46 @@ def product_update_by_id(request, product_id):
         return jsonify({"message": 'product_id is required'}), 400
 
     for record in product_records:
-        print("record: ", record)
         if record['product_id'] == product['product_id']:
             product = record
 
-    print("product: ", product)
-
-    product['name'] = data.get('name', product['name'])
+    product['product_name'] = data.get('product_name', product['product_name'])
     product['description'] = data.get('description', product['description'])
     product['price'] = data.get('price', product['price'])
+    product['active'] = data.get('active', product['active'])
     product_records.append(product)
 
-    return jsonify({"message": f"Product {product['name']} has been updated."}), 200
+    return jsonify({"message": f"Product {product['product_name']} has been updated."}), 200
+
+
+def product_update_active_status(product_id):
+    product = {}
+    product['product_id'] = int(product_id)
+
+    if not product['product_id']:
+        return jsonify({"message": 'product_id is required'}), 400
+
+    for record in product_records:
+        if record['product_id'] == product['product_id']:
+            product = record
+
+    product['active'] = not product['active']
+    product_records.append(product)
+
+    return jsonify({"message": f"Product {product['product_name']}'s active status has been set to {product['active']}."}), 200
+
+
+def product_delete(product_id):
+    product = {}
+    product['product_id'] = int(product_id)
+
+    if not product['product_id']:
+        return jsonify({"message": 'product_id is required'}), 400
+
+    for record in product_records:
+        if record['product_id'] == product['product_id']:
+            product = record
+
+    product_records.remove(product)
+
+    return jsonify({"message": f"Product {product['product_name']} has been deleted."}), 200
--- flask-api-crud/routes/products_routes.py
+++ flask-api-crud/routes/products_routes.py
@@ -11,6 +11,16 @@ def product_create():
     return products_controller.product_create(request)
 
 
+@product.route('/products', methods=['GET'])
+def products_get():
+    return products_controller.products_get()
+
+
+@product.route('/products/active', methods=['GET'])
+def products_get_active():
+    return products_controller.products_get_active()
+
+
 @product.route('/product/<product_id>', methods=['GET'])
 def product_get_by_id(product_id):
     return products_controller.product_get_by_id(product_id)
@@ -19,3 +29,13 @@ def product_get_by_id(product_id):
 @product.route('/product/<product_id>', methods=['PUT'])
 def product_update_by_id(product_id):
     return products_controller.product_update_by_id(request, product_id)
+
+
+@product.route('/product/activity/<product_id>', methods=['PUT'])
+def product_update_active_status(product_id):
+    return products_controller.product_update_active_status(product_id)
+
+
+@product.route('/product/delete/<product_id>', methods=['DELETE'])
+def product_delete(product_id):
+    return products_controller.product_delete(product_id)
