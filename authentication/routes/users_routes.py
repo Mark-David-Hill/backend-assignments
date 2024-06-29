645b76f167ac7df2bce2cac3a5eb7f178d6903a5
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-06-29 12:13:19 -0600

    adds the rest of the user routes for the authentication assignment

--- authentication/routes/users_routes.py
+++ authentication/routes/users_routes.py
@@ -8,3 +8,23 @@ users = Blueprint('user', __name__)
 @users.route("/user", methods=['POST'])
 def user_add():
     return controllers.user_add(request)
+
+
+@users.route('/users')
+def users_get_all():
+    return controllers.users_controller.users_get_all()
+
+
+@users.route('/user/<user_id>')
+def user_get_by_id(user_id):
+    return controllers.users_controller.user_get_by_id(user_id)
+
+
+@users.route('/user/<user_id>', methods=["PUT"])
+def user_update(user_id):
+    return controllers.users_controller.user_update(request, user_id)
+
+
+@users.route('/user/delete/<user_id>', methods=["DELETE"])
+def user_delete(user_id):
+    return controllers.users_controller.user_delete(user_id)
